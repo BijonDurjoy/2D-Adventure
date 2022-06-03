40058cdd935a227ab5a78b6f90650f0e5e6cea16
BIJON <bijon176193@gmail.com>
2022-06-03 11:04:15 +0600

    Blue Boy added to the game

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable
     final int originalTileSize = 16; // 16*16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48*48 tile;
+    public final int tileSize = originalTileSize * scale; // 48*48 tile;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; //768 pix
@@ -19,7 +21,7 @@ public class GamePanel extends JPanel implements Runnable
     int FPS = 60;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-
+    Player player =new Player(this,keyH);
     //set player's default position
     int playerX = 100;
     int playerY = 100;
@@ -69,30 +71,14 @@ public class GamePanel extends JPanel implements Runnable
     }
     public void update()
     {
-        if(keyH.upPressed == true)
-        {
-            playerY -= playerSpreed;
-        }
-        else if(keyH.downPressed ==true)
-        {
-            playerY += playerSpreed;
-        }
-        else if(keyH.leftPressed ==true)
-        {
-            playerX -= playerSpreed;
-        }
-        else if(keyH.rightPressed == true)
-        {
-            playerX += playerSpreed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-        g2.fillRect(playerX,playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
     }
 }
