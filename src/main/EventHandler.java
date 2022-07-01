e37be76f38a2e515469b38685bcb95ff18f24deb
BIJON SAHA <bijon176193@gmail.com>
2022-07-01 21:29:51 +0600

    Event update

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -5,27 +5,37 @@ import java.awt.*;
 public class EventHandler
 {
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    EventRect eventRect[][];
 
     public EventHandler(GamePanel gp)
     {
         this.gp = gp;
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+        int col =0, row=0;
+        while(col < gp.maxWorldCol && row < gp.maxWorldRow)
+        {
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 2;
+            eventRect[col][row].height = 2;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
 
-        eventRect = new Rectangle();
-        eventRect.x = 23;
-        eventRect.y = 23;
-        eventRect.width = 2;
-        eventRect.height = 2;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+            col ++;
+            if(col == gp.maxWorldCol)
+            {
+                col = 0;
+                row ++;
+            }
+        }
     }
 
     public void checkEvent()
     {
         if (hit(27,16,"right") == true)
         {
-            teleport(gp.dialogueState);
+            damagePit(gp.dialogueState);
         }
 
         if(hit(23,12,"up") == true)
@@ -33,16 +43,16 @@ public class EventHandler
             healingPool(gp.dialogueState);
         }
     }
-    public boolean hit (int eventCol, int eventRow, String reqDirection)
+    public boolean hit (int col, int row, String reqDirection)
     {
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if(gp.player.solidArea.intersects(eventRect))
+        if(gp.player.solidArea.intersects(eventRect[col][row]))
         {
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any"))
             {
@@ -52,8 +62,8 @@ public class EventHandler
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
