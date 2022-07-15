d5721374480c8909cc4bab7d4b2fd80d079fecd9
BIJON SAHA <bijon176193@gmail.com>
2022-07-15 14:49:53 +0600

    Sword problem solve!

--- src/object/OBJ_Sword_Normal.java
+++ src/object/OBJ_Sword_Normal.java
@@ -13,6 +13,8 @@ public class OBJ_Sword_Normal extends Entity
         name = "Normal Sword";
         down1 = setup("/objects/sword_normal",gp.tileSize, gp.tileSize);
         attackValue = 4;
+        attackArea.width = 36;
+        attackArea.height = 36;
         description = "["+name+"]\nAn old sword.";
     }
 }
