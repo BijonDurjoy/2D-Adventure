a48dc7568535e4725d500b8f209bc0df9c88fd46
Hamim Rahman <hamimr3823@gmail.com>
2022-08-13 04:38:26 -0700

    Cutting Trees

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -153,6 +153,9 @@ public class  Player extends Entity
             int monsterIndex = gp.cChecker.checkEntity(this,gp.monster);
             contactMonster(monsterIndex);
 
+            //INTERACTIVE TILE COLLISION
+            int iTileIndex = gp.cChecker.checkEntity(this, gp.iTile);
+
             //CHECK EVENT
             gp.eHandler.checkEvent();
 
@@ -257,6 +260,9 @@ public class  Player extends Entity
             int monsterIndex = gp.cChecker.checkEntity(this, gp.monster);
             damageMonster(monsterIndex, attack);
 
+            int iTileIndex = gp.cChecker.checkEntity(this, gp.iTile);
+            damageInteractiveTile(iTileIndex);
+
             //After checking collision restores the original data
             worldX = currentWorldX;
             worldY = currentWorldY;
@@ -343,6 +349,17 @@ public class  Player extends Entity
                    checkLevelUp();
                }
            }
+        }
+    }
+    public void damageInteractiveTile(int i){
+        if(i != 999 && gp.iTile[i].destructible==true && gp.iTile[i].isCorrectItem(this)==true && gp.iTile[i].invincible == false){
+            gp.iTile[i].playSE();
+            gp.iTile[i].life--;
+            gp.iTile[i].invincible = true;
+
+            if(gp.iTile[i].life == 0){
+                gp.iTile[i] = gp.iTile[i].getDestroyedFrom();
+            }
         }
     }
     public void checkLevelUp(){
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import entity.Entity;
 import entity.Player;
 import tile.TileManager;
+import tile_interactive.InteractiveTile;
 
 import javax.swing.*;
 import java.awt.*;
@@ -45,6 +46,7 @@ public class GamePanel extends JPanel implements Runnable
     public Entity obj[] = new Entity[20];
     public Entity npc[] = new Entity[10];
     public Entity monster[] = new Entity[20];
+    public InteractiveTile iTile[] = new InteractiveTile[50];
     public ArrayList<Entity> projectileList = new ArrayList<>();
     ArrayList<Entity> entityList = new ArrayList<>();
 
@@ -69,6 +71,7 @@ public class GamePanel extends JPanel implements Runnable
         aSetter.setObject();
         aSetter.setNPC();
         aSetter.setMonster();
+        aSetter.setInteractiveTile();
         playMusic(0);
         //stopMusic();
         gameState = titleState;
@@ -137,6 +140,11 @@ public class GamePanel extends JPanel implements Runnable
                     }
                 }
             }
+            for(int i = 0;i<iTile.length;i++){
+                if(iTile[i]!=null){
+                    iTile[i].update();
+                }
+            }
         }
         if (gameState == pauseState) {
             //NOTHING
@@ -160,10 +168,14 @@ public class GamePanel extends JPanel implements Runnable
         else{
             //TILE
             tileM.draw(g2);
-
+            //Interactive Tile
+            for(int i = 0;i<iTile.length;i++){
+                if(iTile[i]!=null){
+                    iTile[i].draw(g2);
+                }
+            }
             //ADD ENTITY TO THE LIST
             entityList.add(player);
-
             for (int i =0; i<npc.length; i++)
             {
                 if (npc[i] != null)
